b1b80315cda62cbebac5c90ef8e74b808c184500
vipin-2023 <githublogin321@gmail.com>
2023-07-03 20:18:40 +0530

    model change

--- src/models/TodoModel.ts
+++ src/models/TodoModel.ts
@@ -2,12 +2,20 @@ import mongoose, { Schema, Document } from 'mongoose';
 
 // Define the interface for the Todo document
 interface ITodo extends Document {
-  text: string;
+  text: string
+  date:Date
 }
 
 // Define the Todo schema
 const todoSchema = new Schema<ITodo>({
-  text: { type: String, required: true },
+  text: { 
+    type: String,
+    required: true 
+  },
+  date:{
+    type:Date,
+    default:Date.now
+  }
 });
 
 // Define and export the Todo model
